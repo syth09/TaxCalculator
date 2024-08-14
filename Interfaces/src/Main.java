b0b39e5a2dce349ba9c4561f42cd9a7d519c0312
Monsieur Quinn <136824880+syth09@users.noreply.github.com>
2024-08-14 18:15:22 +0700

    Add files via upload

---
+++ Interfaces/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        var calculator =  new TaxCalculator2018(10_000);
+        var report = new TaxReport(calculator);
+    }
+}
---
+++ Interfaces/src/TaxReport.java
@@ -0,0 +1,12 @@
+public class TaxReport {
+    private TaxCalculator calculator;
+
+    public TaxReport(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void show() {
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+}
